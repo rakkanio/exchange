b12259fa1f88f040a37b50806efbeb555bec7e42
Abdul Barik <abdulbarikmb@gmail.com>
2022-04-09 01:01:11 +0530

    updated ipfs client import

--- server/src/config/ipfs.js
+++ server/src/config/ipfs.js
@@ -1,8 +1,9 @@
-import ipfsClient from 'ipfs-http-client'
+import { create } from 'ipfs-http-client'
+
 const ipfsConnection = async () => {
     try {
-        const ipfs = new ipfsClient({host:process.env.IPFS_HOST, port:process.env.IPFS_PORT,
-             protocol:process.env.IPFS_PROTOCOL})
+        console.log('Connect to client')
+        const ipfs =  create()
         return ipfs
     } catch (e) {
         console.log('Error while connecting with IPFS server', e);
