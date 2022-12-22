6829404534d0e95e342baf3d367d4609e9cd4a27
abdulbarik <abdulbarikmb@gmail.com>
2022-12-22 21:31:43 +0530

    updated new images for new app

--- server/src/config/ipfs.js
+++ server/src/config/ipfs.js
@@ -2,8 +2,8 @@ import { create, CID } from 'ipfs-http-client'
 
 const ipfsConnection = async () => {
     try {
-        console.log(`Connect to client ${process.env.IPFS_HOST}:${process.env.IPFS_PORT}`)
         global.ipfs = create(`${process.env.IPFS_HOST}:${process.env.IPFS_PORT}`)
+        console.log(`Connected to client ${process.env.IPFS_HOST}:${process.env.IPFS_PORT}`)
         global.CID=CID
     } catch (err) {
         console.log('Error while connecting with IPFS server', e)
