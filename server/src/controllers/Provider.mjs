5ab495913ff4bea4d4c0bf2cf42a918be77f679e
abdulbarik <abdulbarikmb@gmail.com>
2023-08-18 15:53:46 +0530

    update

--- server/src/controllers/Provider.mjs
+++ server/src/controllers/Provider.mjs
@@ -1,7 +1,7 @@
 'use strict'
 
 import AlgoAccountModel from "../models/Provider.mjs"
-import XummModel from "../models/xumm"
+import XummModel from "../models/xumm.mjs"
 
 const { generateUrl } = XummModel
 const { fetchAccount, signTransaction, sendTransaction, optIn, validateAccount, updateUserAccout } = AlgoAccountModel
